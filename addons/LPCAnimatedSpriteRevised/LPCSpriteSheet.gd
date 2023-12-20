59486136424a6fa89465d931842d63937ba09755
Mosberg <RasmusTMosberg@gmail.com>
2023-12-20 02:31:16 +0100

    Add files via upload

---
+++ addons/LPCAnimatedSpriteRevised/LPCSpriteSheet.gd
@@ -0,0 +1,9 @@
+#LPCSpriteSheet.gd:
+extends Resource
+class_name LPCSpriteSheet
+
+@export var SpriteSheet:Texture
+@export var Name:String = ""
+
+signal SpriteSheetChanged(texture:Texture)
+signal NameChanged
